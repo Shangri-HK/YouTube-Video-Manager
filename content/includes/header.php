13e68d94f19580cecc6a68f3e930c557a5e27694
Shangri-HK <aloea.heroiin@gmail.com>
2015-12-13 17:34:48 +0100

    slider search

--- content/includes/header.php
+++ content/includes/header.php
@@ -18,10 +18,113 @@ include_once 'config.php';
 
     <script type="text/javascript" src="../js/jquery/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/slider/jssor.slider.mini.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+
+            var jssor_1_options = {
+                $AutoPlay: false,
+                $AutoPlaySteps: 3,
+                $SlideDuration: 160,
+                $SlideWidth: 480,
+                $SlideSpacing: 0,
+                $Cols: 4,
+                $ArrowNavigatorOptions: {
+                    $Class: $JssorArrowNavigator$,
+                    $Steps: 3
+                },
+                $BulletNavigatorOptions: {
+                    $Class: $JssorBulletNavigator$,
+                    $SpacingX: 1,
+                    $SpacingY: 1
+                }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 1920);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
+    </script>
+
+    <style>
+
+        /* jssor slider bullet navigator skin 03 css */
+        /*
+        .jssorb03 div           (normal)
+        .jssorb03 div:hover     (normal mouseover)
+        .jssorb03 .av           (active)
+        .jssorb03 .av:hover     (active mouseover)
+        .jssorb03 .dn           (mousedown)
+        */
+        .jssorb03 {
+            position: absolute;
+        }
+        .jssorb03 div, .jssorb03 div:hover, .jssorb03 .av {
+            position: absolute;
+            /* size of bullet elment */
+            width: 21px;
+            height: 21px;
+            text-align: center;
+            line-height: 21px;
+            color: white;
+            font-size: 12px;
+            background: url('img/b03.png') no-repeat;
+            overflow: hidden;
+            cursor: pointer;
+        }
+        .jssorb03 div { background-position: -5px -4px; }
+        .jssorb03 div:hover, .jssorb03 .av:hover { background-position: -35px -4px; }
+        .jssorb03 .av { background-position: -65px -4px; }
+        .jssorb03 .dn, .jssorb03 .dn:hover { background-position: -95px -4px; }
+
+        /* jssor slider arrow navigator skin 03 css */
+        /*
+        .jssora03l                  (normal)
+        .jssora03r                  (normal)
+        .jssora03l:hover            (normal mouseover)
+        .jssora03r:hover            (normal mouseover)
+        .jssora03l.jssora03ldn      (mousedown)
+        .jssora03r.jssora03rdn      (mousedown)
+        */
+        .jssora03l, .jssora03r {
+            display: block;
+            position: absolute;
+            /* size of arrow element */
+            width: 55px;
+            height: 55px;
+            cursor: pointer;
+            background: url('img/a03.png') no-repeat;
+            overflow: hidden;
+        }
+        .jssora03l { background-position: -3px -33px; }
+        .jssora03r { background-position: -63px -33px; }
+        .jssora03l:hover { background-position: -123px -33px; }
+        .jssora03r:hover { background-position: -183px -33px; }
+        .jssora03l.jssora03ldn { background-position: -243px -33px; }
+        .jssora03r.jssora03rdn { background-position: -303px -33px; }
+    </style>
 
     <link rel="stylesheet" type="text/css" href="../css/bootstrap/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap/override.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
 </head>
 <body>
 
--- content/search.php
+++ content/search.php
@@ -12,7 +12,9 @@ include_once 'includes/header.php';
 $parameters = $_POST['search-query'];
 $parameters = str_replace(' ', '%2B', $parameters);
 
-$query = '?part=snippet&q='.$parameters;
+$maxResults = 9;
+
+$query = '?part=snippet&q='.$parameters.'&maxResults='.$maxResults;
 echo $query =  $_SEARCH_API.$query.$_API_KEY;
 
 $ch = curl_init();
@@ -26,13 +28,45 @@ curl_close($ch);
 
 $resp = json_decode($resp);
 
-var_dump($resp);
+//var_dump($resp);
 
-foreach ($resp->items as $result) {
-    echo 'video ID: '.$result->id->videoId.'<br/>';
-}
 
 ?>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/YhQh5i_dlhA" frameborder="0" allowfullscreen></iframe>
+<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1920px; height: 360px; overflow: hidden; visibility: hidden; background-color: #000000">
+    <!-- Loading Screen -->
+    <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+        <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+        <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+    </div>
+    <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1920px; height: 360px; overflow: hidden;">
+        <?php
+        foreach ($resp->items as $result) {
+            if ($result->id->kind == 'youtube#video') {
+                echo '<div style="display: none">';
+                echo '<h1 style="position: absolute; line-height:36px; top: -17px; left: 15px; color: #ccc;">'.$result->snippet->title.'</h1>';
+                echo '<img src="'.$result->snippet->thumbnails->high->url.'">';
+                echo '</div>';
+            }
+            else if ($result->id->kind == 'youtube#playlist') {
+                echo '<div style="display: none">';
+                echo '<img src="'.$result->snippet->thumbnails->high->url.'">';
+                echo '</div>';
+            }
+        }
+
+        ?>
+    </div>
+    <!-- Bullet Navigator -->
+    <div data-u="navigator" class="jssorb03" style="bottom:10px;right:10px;">
+        <!-- bullet navigator item prototype -->
+        <div data-u="prototype" style="width:21px;height:21px;">
+            <div data-u="numbertemplate"></div>
+        </div>
+    </div>
+    <!-- Arrow Navigator -->
+    <span data-u="arrowleft" class="jssora03l" style="top:0px;left:8px;width:55px;height:55px;" data-autocenter="2"></span>
+    <span data-u="arrowright" class="jssora03r" style="top:0px;right:8px;width:55px;height:55px;" data-autocenter="2"></span>
+    <a href="http://www.jssor.com" style="display:none">Bootstrap Carousel</a>
+</div>
 
